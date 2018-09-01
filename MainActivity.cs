68850240a32c16da8393ad495c584cc31cfe8eef
massimo trovati <massimotrovati92@gmail.com>
2018-09-01 12:07:15 +0200

    code

---
+++ MainActivity.cs
@@ -0,0 +1,107 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using Android.Speech.Tts;
+using Android.Content;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace parlalo6._0
+{
+    [Activity(MainLauncher = true)]
+    public class MainActivity : Activity, TextToSpeech.IOnInitListener
+    {
+        TextToSpeech textToSpeech;
+        Context context;
+        private readonly int miocodice = 101, NeedLang = 103;
+        Java.Util.Locale lang;
+
+
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_main);
+            var btnSayIt = FindViewById<Button>(Resource.Id.btnSpeak);
+            var editWhatToSay = FindViewById<EditText>(Resource.Id.editSpeech);
+            var spinLanguages = FindViewById<Spinner>(Resource.Id.spinLanguage);
+            var txtSpeedVal = FindViewById<TextView>(Resource.Id.textSpeed);
+            var txtPitchVal = FindViewById<TextView>(Resource.Id.textPitch);
+            var seekSpeed = FindViewById<SeekBar>(Resource.Id.seekSpeed);
+            var seekPitch = FindViewById<SeekBar>(Resource.Id.seekPitch);
+            seekSpeed.Progress = seekPitch.Progress = 127;
+            txtSpeedVal.Text = txtPitchVal.Text = "0.5";
+            context = btnSayIt.Context;
+            textToSpeech = new TextToSpeech(this, this, "com.google.android.tts");
+            var langAvailable = new List<string> { "Default" };
+            var localesAvailable = Java.Util.Locale.GetAvailableLocales().ToList();
+            foreach (var locale in localesAvailable)
+            {
+                LanguageAvailableResult res = textToSpeech.IsLanguageAvailable(locale);
+                switch (res)
+                {
+                    case LanguageAvailableResult.Available:
+                        langAvailable.Add(locale.DisplayLanguage);
+                        break;
+                    case LanguageAvailableResult.CountryAvailable:
+                         langAvailable.Add(locale.DisplayLanguage);
+                        break;
+                    case LanguageAvailableResult.CountryVarAvailable:
+                         langAvailable.Add(locale.DisplayLanguage);
+                        break;
+                        }
+
+            }
+
+            langAvailable = langAvailable.OrderBy(t => t).Distinct().ToList();
+            var adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerDropDownItem, langAvailable);
+            spinLanguages.Adapter = adapter;
+            lang = Java.Util.Locale.Default;
+            textToSpeech.SetLanguage(lang);
+            textToSpeech.SetPitch(0.5f);
+            textToSpeech.SetSpeechRate(0.5f);
+            btnSayIt.Click += delegate
+            {
+                if (!string.IsNullOrEmpty(editWhatToSay.Text))
+                    textToSpeech.Speak(editWhatToSay.Text, QueueMode.Flush, null);
+            };
+            seekPitch.StopTrackingTouch += (object sender, SeekBar.StopTrackingTouchEventArgs e) =>
+            {
+                var seek = sender as SeekBar;
+                var progress = seek.Progress / 255f;
+                textToSpeech.SetPitch(progress);
+                txtPitchVal.Text = progress.ToString("F2");
+            };
+            seekSpeed.StopTrackingTouch += (object sender, SeekBar.StopTrackingTouchEventArgs e) =>
+            {
+                var seek = sender as SeekBar;
+                var progress = seek.Progress / 255f;
+                textToSpeech.SetSpeechRate(progress);
+                txtSpeedVal.Text = progress.ToString("F2");
+            };
+            spinLanguages.ItemSelected += (object sender, AdapterView.ItemSelectedEventArgs e) =>
+            {
+                lang = Java.Util.Locale.GetAvailableLocales().FirstOrDefault(t => t.DisplayLanguage == langAvailable[(int)e.Id]);
+                var checkTTSIntent = new Intent();
+                checkTTSIntent.SetAction(TextToSpeech.Engine.ActionCheckTtsData);
+                StartActivityForResult(checkTTSIntent, NeedLang);
+            };
+        }
+        void TextToSpeech.IOnInitListener.OnInit(OperationResult status)
+        {
+            if (status == OperationResult.Error)
+                textToSpeech.SetLanguage(Java.Util.Locale.Default);
+            if (status == OperationResult.Success)
+                textToSpeech.SetLanguage(lang);
+                   }
+        protected override void OnActivityResult(int req, Result res, Intent data)
+        {
+            if (req == NeedLang)
+            {
+                var installTTS = new Intent();
+                installTTS.SetAction(TextToSpeech.Engine.ActionInstallTtsData);
+                StartActivity(installTTS);
+                            }
+                    }
+            }
+}
---
+++ obj/Debug/MonoAndroid81/android/src/md5eed66a31de682a3cfee8f35195696c5e/MainActivity.java
@@ -0,0 +1,66 @@
+package md5eed66a31de682a3cfee8f35195696c5e;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer,
+		android.speech.tts.TextToSpeech.OnInitListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onActivityResult:(IILandroid/content/Intent;)V:GetOnActivityResult_IILandroid_content_Intent_Handler\n" +
+			"n_onInit:(I)V:GetOnInit_IHandler:Android.Speech.Tts.TextToSpeech/IOnInitListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"";
+		mono.android.Runtime.register ("parlalo6._0.MainActivity, parlalo6.0", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("parlalo6._0.MainActivity, parlalo6.0", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onActivityResult (int p0, int p1, android.content.Intent p2)
+	{
+		n_onActivityResult (p0, p1, p2);
+	}
+
+	private native void n_onActivityResult (int p0, int p1, android.content.Intent p2);
+
+
+	public void onInit (int p0)
+	{
+		n_onInit (p0);
+	}
+
+	private native void n_onInit (int p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
